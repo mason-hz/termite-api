b9922e850e637d42cabe649f8a6a099a4d765b7a
aelf-hz <mason.huang@aelf.io>
2021-06-17 16:25:42 +0800

    feat: 🎸 update address

--- app/constants/index.js
+++ app/constants/index.js
@@ -4,12 +4,9 @@ const config = {
   rpcUrl: 'https://eth-kovan.alchemyapi.io/v2/xySGYPfffRK4wIUO1AqWgwgi2-Q2O1wy',
   apolloUrl: 'https://api.thegraph.com/subgraphs/name/zhxymh/svault-kovan',
 
-  Controller: '0x07eAe6689403E6737719CFB4d24aa8f7F0A31Aca',
-  WETH_ADDRESS: '0xA050886815CFc52a24B9C4aD044ca199990B6690',
-  SwapFactory: '0x8017f90d1332cc63Ff9Cc32F4461edd85F537b56',
-  SwapRouter: '0xC4ED01EeB878aa2cAFDC1EB60C88d84187d2ab51',
-  SVaultNetValue: '0xC1Adef553c3Efea8923AC6333Fb1b70AB5507391',
-  PriceView: '0xBBc31dfE4effCFB8a9D04896a79aA5f15013526b',
+  Controller: '0xd98cf5C109aA130556eb81344c6303B6fe99f722',
+  SVaultNetValue: '0x2A28F3Dc81869Ce0dE70edC27777fbB5189c0f72',
+  PriceView: '0x9219f24F6B7D5AccF6C6d34044937EF882948b1e',
 
   blockDay: 21600,
   blockPerYear: 21600 * 365,
